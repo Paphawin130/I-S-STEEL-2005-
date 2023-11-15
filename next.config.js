84b7711997bdeee03e2c42a1ae92adcc90de7abf
Tanny <69077943+Paphawin130@users.noreply.github.com>
2023-11-15 22:28:49 +0700

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
+  async redirects() {
+    return [];
+  },
+};
+
+module.exports = {
+  images: {
+    unoptimized: true,
+  },
+};
